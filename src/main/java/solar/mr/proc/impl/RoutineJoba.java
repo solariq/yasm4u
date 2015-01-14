58b491958cb2fce063f788f1588a83a3434c52a3
Vasily Levchenko <minamoto@yandex-team.ru>
2015-01-14 13:00:30 +0300

    RoutineJoba.java (reverted merge).

--- src/main/java/solar/mr/proc/impl/RoutineJoba.java
+++ src/main/java/solar/mr/proc/impl/RoutineJoba.java
@@ -7,6 +7,8 @@ import solar.mr.proc.Joba;
 import solar.mr.proc.Whiteboard;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
 * User: solar
@@ -46,14 +48,27 @@ public class RoutineJoba implements Joba {
     }
 
     final MethodRoutineBuilder builder = new MethodRoutineBuilder();
-    builder.addInput(input);
-    builder.addOutput(output);
+    builder.addInput(resolveAll(input, wb));
+    builder.addOutput(resolveAll(output, wb));
     builder.setState(wb.snapshot());
     builder.setMethodName(method.getName());
     builder.setType(type);
     builder.setRoutineClass(method.getDeclaringClass());
     builder.complete();
     return wb.env().execute(builder, wb.errorsHandler());
+  }
+
+  private String[] resolveAll(String[] input, Whiteboard wb) {
+    final List<String> result = new ArrayList<>(input.length);
+    for(int i = 0; i < input.length; i++) {
+      wb.processAs(input[i], new Processor<MRTableShard>() {
+        @Override
+        public void process(MRTableShard shard) {
+          result.add(shard.path());
+        }
+      });
+    }
+    return result.toArray(new String[result.size()]);
   }
 
   @Override
