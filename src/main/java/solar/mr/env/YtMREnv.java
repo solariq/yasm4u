7bc6ad48d7b4f9f3075e76bbb6be1e97a57de37c
Vasily Levchenko <minamoto@yandex-team.ru>
2015-02-06 09:14:28 +0300

    YtMREnv:: Ultra-fast operations NONE -> COMPLETED

--- src/main/java/solar/mr/env/YtMREnv.java
+++ src/main/java/solar/mr/env/YtMREnv.java
@@ -635,7 +635,8 @@ public class YtMREnv extends RemoteMREnv {
       if (CharSeqTools.equals(arg, TOK_OP_COMPLETED)
           && (status == OperationStatus.INITIALIZING
           || status == OperationStatus.PREPARING
-          || status == OperationStatus.PRINT_HINT)){
+          || status == OperationStatus.PRINT_HINT
+          || status == OperationStatus.NONE /* Ultra fast operation usually with empty inputs */)){
         status = OperationStatus.COMPETED;
         return;
       }
