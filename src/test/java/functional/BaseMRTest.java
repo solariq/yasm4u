079ffbb24048d34ece860666418ad5993530d0a5
Vasily Levchenko <minamoto@yandex-team.ru>
2015-02-11 19:49:32 +0300

    some test configuration.

--- src/test/java/functional/BaseMRTest.java
+++ src/test/java/functional/BaseMRTest.java
@@ -18,12 +18,12 @@ public class BaseMRTest {
    * that not a problem in YaMR. 
    */
   public static final String TABLE_NAME_PREFIX = "mr:///home/mobilesearch/yasm4u-tests/";
-  public static final String SALT = "_test"; // Don't know how to implement this better yet
+  public static final String SALT = "_test1"; // Don't know how to implement this better yet
 
-//  public static final ProcessRunner YAMR_RUNNER = new SSHProcessRunner("batista", "/Berkanavt/mapreduce/bin/mapreduce-dev");
+  public static final ProcessRunner YAMR_RUNNER = new SSHProcessRunner("batista", "/Berkanavt/mapreduce/bin/mapreduce-dev");
   public static final String YAMR_USER = "mobilesearch";
   public static final String YAMR_CLUSTER = "cedar:8013";
-//  public static final ProcessRunner YTMR_RUNNER = new SSHProcessRunner("testing.mobsearch.serp.yandex.ru", "/usr/bin/yt");
+  //public static final ProcessRunner YTMR_RUNNER = new SSHProcessRunner("prod1-test-mob.serp.yandex.ru", "/usr/bin/yt");
   public static final String YTMR_USER = "minamoto";
   public static final String YTMR_CLUSTER = "plato.yt.yandex.net";
 
@@ -31,9 +31,9 @@ public class BaseMRTest {
   public static Collection<Object[]> data() {
     return Arrays.asList(new Object[][]{
         {LocalMREnv.createTemp()},
-//        {new CompositeMREnv(new YaMREnv(YAMR_RUNNER, YAMR_USER, YAMR_CLUSTER))},
+       {new CompositeMREnv(new YaMREnv(YAMR_RUNNER, YAMR_USER, YAMR_CLUSTER))},
         //{new ProfilerMREnv(new YaMREnv(YAMR_RUNNER, YAMR_USER, YAMR_CLUSTER))},
-//        {new YtMREnv(YTMR_RUNNER, YTMR_USER, YTMR_CLUSTER)}
+        //{new CompositeMREnv(new YtMREnv(YTMR_RUNNER, YTMR_USER, YTMR_CLUSTER))}
     });
   }
 
