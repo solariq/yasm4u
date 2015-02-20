551af4ff1e2d127ebbd09d15861e74caf57ea3f6
Vasily Levchenko <minamoto@yandex-team.ru>
2015-02-20 17:45:25 +0300

    dumping remote exception to STDERR.

--- src/main/java/solar/mr/env/ErrorsTableHandler.java
+++ src/main/java/solar/mr/env/ErrorsTableHandler.java
@@ -6,6 +6,10 @@ import solar.mr.MRRoutine;
 import solar.mr.proc.impl.MRPath;
 import solar.mr.routines.MRRecord;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 /**
 * User: solar
 * Date: 30.01.15
@@ -23,7 +27,18 @@ public class ErrorsTableHandler extends MRRoutine {
   public void process(final MRRecord record) {
     CharSequence[] parts = CharSeqTools.split(record.value, '\t', new CharSequence[4]);
     errorsHandler.error(record.key, record.sub, new MRRecord(MRPath.create(parts[0].toString()), parts[1].toString(), parts[2].toString(), parts[3]));
-    System.err.println(record.value);
-    System.err.println(record.key + "\t" + record.sub.replace("\\n", "\n").replace("\\t", "\t"));
+    try {
+      final Class clazz = Class.forName(record.key);
+      if (clazz.isAssignableFrom(Throwable.class)){
+        Throwable th = (Throwable)new ObjectInputStream(new ByteArrayInputStream(CharSeqTools.parseBase64(record.value))).readObject();
+        th.printStackTrace(System.err);
+      }
+    } catch (ClassNotFoundException e) {
+      System.err.println(record.value);
+      System.err.println(record.key + "\t" + record.sub.replace("\\n", "\n").replace("\\t", "\t"));
+    }
+    catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
