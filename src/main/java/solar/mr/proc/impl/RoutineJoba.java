solariq/yasm4u

commit c3840d117708e351fbdc49712cda9e695c8c8148

performance issues fixed

src/main/java/solar/mr/proc/impl/RoutineJoba.java
@@ -40,22 +40,22 @@ public class RoutineJoba implements Joba {
 
   @Override
   public boolean run(final Whiteboard wb) {
+    final MRPath[] inputResolved = resolveAll(input, wb);
+    final MRPath[] outputResolved = resolveAll(output, wb);
     if (type == MRRoutineBuilder.RoutineType.REDUCE) {
-      for (int i = 0; i < input.length; i++) {
-        final String resourceName = input[i];
-        wb.processAs(resourceName, new Processor<MRPath>() {
-          @Override
-          public void process(MRPath shard) {
-            wb.env().sort(new MRPath(shard.mount, shard.path, false));
-            wb.set(resourceName, new MRPath(shard.mount, shard.path, true));
-          }
-        });
+      for (int i = 0; i < inputResolved.length; i++) {
+        final MRPath resourceName = inputResolved[i];
+        if (!resourceName.sorted) {
+          final MRPath sortedPath = new MRPath(resourceName.mount, resourceName.path, false);
+          wb.env().sort(sortedPath);
+          inputResolved[i] = sortedPath;
+        }
       }
     }
 
     final MethodRoutineBuilder builder = new MethodRoutineBuilder();
-    builder.addInput(resolveAll(input, wb));
-    builder.addOutput(resolveAll(output, wb));
+    builder.addInput(inputResolved);
+    builder.addOutput(outputResolved);
     builder.setState(wb.snapshot());
     builder.setMethodName(method.getName());
     builder.setType(type);

src/main/java/solar/mr/env/CompositeMREnv.java
@@ -76,14 +76,21 @@ public class CompositeMREnv implements MREnv {
       if (outputExistence) {
         boolean ffDisabled = Boolean.getBoolean("yasm4u.disableFF");
         System.out.println("Fast forwarding execution " + (ffDisabled? "(ignored)" : "") + builder.toString());
-        if (!ffDisabled)
+        if (!ffDisabled) {
+          final MRTableState[] outAfter = original.resolveAll(out, false);
+          for(int i = 0; i < out.length; i++) {
+            setCopy(out[i], outAfter[i], localOutAfter[i]);
+          }
+
           return true;
+        }
       }
     }
     final MultiMap<MRPath, CharSequence> needToAddToSample = new MultiMap<>();
     try {
-      if (!original.execute(builder, new MRErrorsHandler() {
+      final boolean rc = original.execute(builder, new MRErrorsHandler() {
         int counter = 0;
+
         @Override
         public void error(String type, String cause, MRRecord record) {
           needToAddToSample.put(record.source, record.toString());
@@ -102,8 +109,13 @@ public class CompositeMREnv implements MREnv {
         public int errorsCount() {
           return counter;
         }
-      }, jar))
+      }, jar);
+      if (!rc) {
+        for(int i = 0; i < out.length; i++) {
+          setCopy(out[i], null, null);
+        }
         return false;
+      }
       final MRTableState[] outAfter = original.resolveAll(out, false);
       for(int i = 0; i < out.length; i++) {
         setCopy(out[i], outAfter[i], localOutAfter[i]);

src/main/java/solar/mr/env/LocalMREnv.java
@@ -222,7 +222,7 @@ public class LocalMREnv implements MREnv {
     read(shard, new Processor<MRRecord>() {
       @Override
       public void process(final MRRecord arg) {
-        sort.add(Pair.create(arg.key + '\t' + arg.value, arg));
+        sort.add(Pair.create(arg.key + '\t' + arg.sub, arg));
       }
     });
     Collections.sort(sort, new Comparator<Pair<String, MRRecord>>() {

src/main/java/solar/mr/env/RemoteMREnv.java
@@ -88,6 +88,8 @@ public abstract class RemoteMREnv implements MREnv {
 
       final MRPath[] input = builder.input();
       for (int i = 0; i < input.length; i++) {
+        to.append(Integer.toString(i)).append("\n");
+        to.flush();
         env.sample(input[i], new Processor<MRRecord>() {
           @Override
           public void process(MRRecord arg) {
@@ -247,7 +249,7 @@ public abstract class RemoteMREnv implements MREnv {
     while (itPath.hasNext()) {
       final MRPath path = itPath.next();
       final MRPath parent = path.parent();
-      if (parent.isRoot() && isFat(path)) {
+      if (parent.isRoot() || isFat(path)) {
         result.add(path);
         itPath.remove();
       }

src/main/java/solar/mr/env/YtMREnv.java
@@ -396,11 +396,10 @@ public class YtMREnv extends RemoteMREnv {
 
   @Override
   protected boolean isFat(MRPath path) {
-    /*final String localPath = localPath(path);
+    final String localPath = localPath(path);
     if ("//home/mobilesearch/logprocessing.daily/paradiso/".equals(localPath))
       return true;
-    return false;*/
-    return path.isDirectory();
+    return false;
   }
 
   @Override

commit 130a8a9cfae287aa0035d3fee2388ffc60dbb42e

Fix of bug the introduced in  c3840d117708e351fbdc49712cda9e695c8c8148. instead of sorted path returned unsorted after sort operation.

src/main/java/solar/mr/proc/impl/RoutineJoba.java
@@ -46,8 +46,8 @@ public class RoutineJoba implements Joba {
       for (int i = 0; i < inputResolved.length; i++) {
         final MRPath resourceName = inputResolved[i];
         if (!resourceName.sorted) {
-          final MRPath sortedPath = new MRPath(resourceName.mount, resourceName.path, false);
-          wb.env().sort(sortedPath);
+          wb.env().sort(resourceName);
+          final MRPath sortedPath = new MRPath(resourceName.mount, resourceName.path, true);
           inputResolved[i] = sortedPath;
         }
       }