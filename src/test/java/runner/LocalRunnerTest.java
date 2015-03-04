f078639cd611b128507de668df0a6aa84175911d
Vasily Levchenko <minamoto@yandex-team.ru>
2015-03-04 21:44:39 +0300

    LocalRunnerTest

--- src/test/java/runner/LocalRunnerTest.java
+++ src/test/java/runner/LocalRunnerTest.java
@@ -1,7 +1,37 @@
 package runner;
 
+import org.junit.Assert;
+import org.junit.Test;
+import solar.mr.env.LocalMREnv;
+import solar.mr.env.LocalProcessRunner;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by minamoto on 04/03/15.
  */
 public class LocalRunnerTest {
+  @Test
+  public void shouldNotFail() throws IOException, InterruptedException {
+    LocalProcessRunner runner = new LocalProcessRunner("echo");
+    Process proc = runner.start("ok");
+    LineNumberReader reader = new LineNumberReader(new InputStreamReader(proc.getInputStream()));
+    assertEquals("ok", reader.readLine());
+    int rc = proc.waitFor();
+    assertEquals(rc, 0);
+  }
+
+  @Test
+  public void shouldNotFailManyParams() throws IOException, InterruptedException {
+    LocalProcessRunner runner = new LocalProcessRunner("echo");
+    Process proc = runner.start("1", "2", "3", "4");
+    LineNumberReader reader = new LineNumberReader(new InputStreamReader(proc.getInputStream()));
+    assertEquals("1 2 3 4", reader.readLine());
+    int rc = proc.waitFor();
+    assertEquals(rc, 0);
+  }
 }
