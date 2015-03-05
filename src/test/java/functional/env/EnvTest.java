6f7c141e60f369a023e054631bd0b7a3a16a7b02
Vasily Levchenko <minamoto@yandex-team.ru>
2015-03-05 22:04:33 +0300

    mixed tests

--- src/test/java/functional/env/EnvTest.java
+++ src/test/java/functional/env/EnvTest.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import solar.mr.MRTableState;
+import solar.mr.proc.Whiteboard;
 import solar.mr.proc.impl.MRPath;
+import solar.mr.proc.impl.WhiteboardImpl;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
@@ -25,5 +28,15 @@ public class EnvTest extends BaseMRTest {
   @Test(expected = IllegalArgumentException.class)
   public void resolveNotExistantDirectory() {
     env.resolve(MRPath.create("/tmp/__not_exists/"));
+  }
+
+  @Test
+  public void conversions() {
+    Whiteboard wb = new WhiteboardImpl(env, "conversions");
+    wb.<Long>set("var:long", 1l);
+    assertEquals(1l, wb.snapshot().get("var:long"));
+
+    wb.<Long>set("var:long",6 * 1l);
+    assertEquals(6l, wb.snapshot().get("var:long"));
   }
 }
