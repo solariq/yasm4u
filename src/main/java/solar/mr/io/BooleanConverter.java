7d1df041ac9993c319134707f1a9da6eee2647a0
Vasily Levchenko <minamoto@yandex-team.ru>
2015-03-11 10:27:39 +0300

    BooleanConverter.

---
+++ src/main/java/solar/mr/io/BooleanConverter.java
@@ -0,0 +1,33 @@
+package solar.mr.io;
+
+import com.spbsu.commons.func.types.ConversionPack;
+import com.spbsu.commons.func.types.TypeConverter;
+import com.spbsu.commons.seq.CharSeqTools;
+
+/**
+ * Created by minamoto on 11/03/15.
+ */
+public class BooleanConverter implements ConversionPack<Boolean,CharSequence> {
+  public static class To implements TypeConverter<Boolean, CharSequence> {
+    @Override
+    public CharSequence convert(Boolean from) {
+      return from? "true" : "false";
+    }
+  }
+  public static class From implements TypeConverter<CharSequence, Boolean> {
+
+    @Override
+    public Boolean convert(CharSequence from) {
+      return !CharSeqTools.toLowerCase(from).equals("false");
+    }
+  }
+  @Override
+  public Class<? extends TypeConverter<Boolean, CharSequence>> to() {
+    return To.class;
+  }
+
+  @Override
+  public Class<? extends TypeConverter<CharSequence, Boolean>> from() {
+    return From.class;
+  }
+}
