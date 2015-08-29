solariq/yasm4u

commit 794714cd9dcc3b4e774679bc018d56f2c36961d5

exception on emty name file creation

src/main/java/ru/yandex/se/yasm4u/domains/mr/env/LocalMREnv.java
@@ -294,6 +294,7 @@ public class LocalMREnv extends MREnvBase {
 
   private long modtime(File file) {
     try {
+      if (file.getName().equals(".txt")) throw new RuntimeException("");
       final BasicFileAttributes attributes = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
       return attributes.lastModifiedTime().toMillis();
     } catch (IOException e) {

commit da7f75b20244cc3e309b5794258c4d9dfdf10087

placed exception throwing introduced in 794714cd9dcc3b4e774679bc018d56f2c36961d5 upper in the stack

src/main/java/ru/yandex/se/yasm4u/domains/mr/env/LocalMREnv.java
@@ -98,6 +98,10 @@ public class LocalMREnv extends MREnvBase {
     if (path.isDirectory())
       throw new IllegalArgumentException("Path must not be directory");
     final File file = file(path);
+    if (file.getName().equals(".txt")) {
+      System.err.println("path: " + path);
+      throw new RuntimeException("empty file name");
+    }
     if (file.exists()) {
       final long[] recordsAndKeys = countRecordsAndKeys(file);
       return new MRTableState(file.getAbsolutePath(), true, path.sorted, crc(file), length(file), recordsAndKeys[1], recordsAndKeys[0], modtime(file), System.currentTimeMillis());
@@ -294,7 +298,6 @@ public class LocalMREnv extends MREnvBase {
 
   private long modtime(File file) {
     try {
-      if (file.getName().equals(".txt")) throw new RuntimeException("");
       final BasicFileAttributes attributes = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
       return attributes.lastModifiedTime().toMillis();
     } catch (IOException e) {