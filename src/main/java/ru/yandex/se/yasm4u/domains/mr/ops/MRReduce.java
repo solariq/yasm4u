a95c094c512c4b40521bd9296c927dce13e415d8
Vasily Levchenko <minamoto@yandex-team.ru>
2015-10-24 16:32:46 +0300

    Revert "send poison-pill at the end of the operation"
    This reverts commit b9d0ad9d18c8786f4f4b3e52620460ce96ee3221.

--- src/main/java/ru/yandex/se/yasm4u/domains/mr/ops/MRReduce.java
+++ src/main/java/ru/yandex/se/yasm4u/domains/mr/ops/MRReduce.java
@@ -96,7 +96,6 @@ public abstract class MRReduce extends MROperation {
           catch (InterruptedException e) {
             //
           }
-          ((MROutputBase) output).stop();
         }
       }
     }, "Reduce thread");
