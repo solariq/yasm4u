fc9d8a3e95a73e66b4088044fbee1dc17d660883
Vasily Levchenko <minamoto@yandex-team.ru>
2015-10-25 09:33:17 +0300

    send stop on end of iterations

--- src/main/java/ru/yandex/se/yasm4u/domains/mr/ops/MRReduce.java
+++ src/main/java/ru/yandex/se/yasm4u/domains/mr/ops/MRReduce.java
@@ -92,6 +92,7 @@ public abstract class MRReduce extends MROperation {
             }
             while (reduceIterator.hasNext())
               reduceIterator.next();
+            ((MROutputBase) output).stop();
           }
           catch (InterruptedException e) {
             //
